cb7cae211c22a1feba676f1f6e9d584a9c10a90b
yudhishthirr <officialyudhishthir@gmail.com>
2024-03-29 10:11:43 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,45 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+        {
+          protocol: "https",
+          hostname: "utfs.io",
+        },
+        // {
+        //   protocol: 'https',
+        //   hostname: 'utfs.io',
+        //   port: '', // Leave empty if not applicable
+        //   pathname: '/your-image-path/**', // Adjust as needed
+        // },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
